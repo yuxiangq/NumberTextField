bd9379b952cbe9f2dbff25720c44e3fc83c0b4e7
yuxiang <yuxiang.q@hotmail.com>
2014-12-19 20:49:30 +0800

    初始化工程

---
+++ NumberTextFieldDemo/NumberTextFieldDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  NumberTextFieldDemo
+//
+//  Created by Qin Yuxiang on 12/19/14.
+//  Copyright (c) 2014 YuxiangQ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
